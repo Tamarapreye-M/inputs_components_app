2e449a72f030e9bce8d2bb3661774af09d22ee4e
Tamarapreye-M <preyemichael116@gmail.com>
2023-01-01 22:30:28 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import { type } from "@testing-library/user-event/dist/type";
+import React from "react";
+import "./App.css";
+import Input from "./components/Input";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const props = [
+	{ default: "" },
+	{ error: true },
+	{ disabled: true },
+	{ helperText: "Some interesting text" },
+	{ helperText: "Some interesting text", error: true },
+	{ startIcon: true },
+	{ endIcon: true },
+	{ fullWidth: true },
+];
+
+const pass = props.map((prop) => {
+	let keys = Object.keys(prop);
+	console.log(keys);
+	return (
+		<Input
+			key={keys}
+			name={keys[0]}
+			value={keys[0] == "helperText" ? Object.values(prop)[0] : null}
+			type={Object.values(prop).length > 1 && "helperError"}
+		/>
+	);
+});
+
+const App = () => {
+	return (
+		<div className="container">
+			<div>{pass}</div>
+		</div>
+	);
+};
 
 export default App;
---
+++ src/components/Input.js
@@ -0,0 +1,22 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const Input = (props) => {
+	console.log(props);
+	let { name, type, value } = props;
+	return (
+		<div className="wrapper">
+			<h2>
+				{name} {type && "with an error"}
+			</h2>
+			<input type="text" placeholder="placeholder" className={type || name} />
+			{name == "helperText" && (
+				<p className={type == "helperError" && "errorText"}>{value}</p>
+			)}
+		</div>
+	);
+};
+
+Input.propTypes = {};
+
+export default Input;
