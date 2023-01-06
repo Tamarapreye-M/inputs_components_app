c584c8b5086b05f692c833e3a2ff0808ef55586d
Tamarapreye-M <preyemichael116@gmail.com>
2023-01-06 23:21:09 +0100

    clean up components

--- src/components/Input.js
+++ src/components/Input.js
@@ -1,5 +1,4 @@
-import React, { useState, useEffect } from "react";
-import PropTypes from "prop-types";
+import React from "react";
 
 const Input = (props) => {
 	console.log(props);
@@ -25,7 +24,5 @@ const Input = (props) => {
 		</div>
 	);
 };
-
-Input.propTypes = {};
 
 export default Input;
